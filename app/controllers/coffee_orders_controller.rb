35ab7a1685a38db18ee704c6564354e2fe591487
Meredith <meredith@mcintosh.io>
2021-02-17 10:02:40 -0500

    backend

--- app/controllers/coffee_orders_controller.rb
+++ app/controllers/coffee_orders_controller.rb
@@ -1,2 +1,24 @@
 class CoffeeOrdersController < ApplicationController
+    def index
+        render json: CoffeeOrder.all.to_json(coffee_order_serializer_option)
+        end
+    
+        def show 
+            coffee_order = CoffeeOrder.find(params[:id])
+            render json: coffee_order.to_json(coffee_order_serializer_option)
+        end 
+
+        private
+
+        def coffee_order_serializer_option
+            {
+                :include => {
+                    :comments => {
+                        :except => [:created_at, :updated_at]
+                    }
+                },
+                :except => [:created_at, :updated_at]
+            }
+    
+        end 
 end
